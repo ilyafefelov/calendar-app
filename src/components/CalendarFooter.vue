af0b5717081a7f5c381ba525a7a960a2c66126d2
ilyafefelov <ilyafefelov@gmail.com>
2024-07-07 19:34:01 +0300

    chore: Refactor CalendarFooter component
    This commit refactors the CalendarFooter component by moving the conditional rendering logic for the content and editable areas. The content area is now always rendered, while the editable area is conditionally rendered based on the "isEditing" flag. This change improves the readability and maintainability of the component code.

--- src/components/CalendarFooter.vue
+++ src/components/CalendarFooter.vue
@@ -1,11 +1,6 @@
 <template>
     <v-footer>
         <v-container fluid class="mt-8">
-            <div v-if="!isEditing" v-html="content"></div>
-            <div v-else >
-                <v-textarea v-model="editableContent"
-                    label="Footer Content"></v-textarea>
-            </div>
             <v-btn variant="outlined" title="Edit Footer"
             :class="[
                 'hover:text-blue-900 text-blue-200 absolute top-0 transition-transform mt-2 backdrop-blur-sm bg-white/10 hover:bg-blue-200',
@@ -14,6 +9,11 @@
                   prepend-icon="mdi-pencil" @click="toggleEdit">
                 {{ isEditing ? 'Save' : '' }}
             </v-btn>
+            <div v-if="!isEditing" v-html="content"></div>
+            <div v-else >
+                <v-textarea v-model="editableContent"
+                    label="Footer Content"></v-textarea>
+            </div>
         </v-container>
     </v-footer>
 </template>
