f08d4f41362590ec2d6e6908fcf658c5cd0f88e6
ilyafefelov <ilyafefelov@gmail.com>
2024-07-07 19:18:27 +0300

    chore: Update Vuetify import in vuetify.ts
    This commit updates the import statement for Vuetify in the vuetify.ts file. The previous import statement was commented out, and the new import statement is used to import the Vuetify styles and createVuetify function. This change ensures that the Vuetify styles are properly imported and applied to the project.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,21 +4,7 @@
  * Framework documentation: https://vuetifyjs.com`
  */
 
-// // Styles
-// import '@mdi/font/css/materialdesignicons.css'
-// import 'vuetify/styles'
 
-// // Composables
-// import { createVuetify } from 'vuetify'
-
-// // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-// export default createVuetify({
-//   theme: {
-//     defaultTheme: 'dark',
-//   },
-// })
-
-// src/plugins/vuetify.ts
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
